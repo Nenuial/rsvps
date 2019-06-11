9bff4985227ae076dbfebe08b757414ec066f08a
Nenuial <pascal.burkhard@gmail.com>
2019-06-11 16:10:00 +0200

    bug fixes and roxygen

--- R/fnch_horses.R
+++ R/fnch_horses.R
@@ -6,7 +6,7 @@
 #' @export
 get_fnch_horse_jumping_results <- function(horseid) {
 
-  url <- glue::glue("https://info.fnch.ch/resultate/pferde/{horse}.json?limit=1000&tab=springen")
+  url <- glue::glue("https://info.fnch.ch/resultate/pferde/{horseid}.json?limit=1000&tab=springen")
   res <- jsonlite::fromJSON(url)
 
   if (length(res$resultate) == 0) return()

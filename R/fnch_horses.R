9066afa0f07c95420f95ea5065f761a11c441c5f
Nenuial <pascal.burkhard@gmail.com>
2019-06-11 17:05:43 +0200

    add gwp functions

--- R/fnch_horses.R
+++ R/fnch_horses.R
@@ -1,3 +1,17 @@
+#' Get gwp infos for horseid
+#'
+#' @param horseid A passport id for a horse
+#'
+#' @return A dataframe of gwp data
+#' @export
+get_fnch_horse_gwp <- function(horseid) {
+  url <- glue::glue("https://info.fnch.ch/resultate/pferde/{horseid}.json?tab=gwp")
+  info <- jsonlite::fromJSON(url) %>%
+    dplyr::select(1:2)
+
+  return(info)
+}
+
 #' Get jumping results for horseid
 #'
 #' @param horseid A passport id for a horse
@@ -15,3 +29,4 @@ get_fnch_horse_jumping_results <- function(horseid) {
 
   return(res)
 }
+
--- R/fnch_riders.R
+++ R/fnch_riders.R
@@ -1,3 +1,30 @@
+#' Get general infos for riderid
+#'
+#' @param riderid A licence id for a rider
+#'
+#' @return A list of rider infos
+#' @export
+get_fnch_rider_infos <- function(riderid) {
+  url <- glue::glue("https://info.fnch.ch/resultate/reiter/{riderid}.json")
+  info <- jsonlite::fromJSON(url)
+
+  return(info$reiter)
+}
+
+#' Get gwp infos for riderid
+#'
+#' @param riderid A licence id for a rider
+#'
+#' @return A dataframe of gwp data
+#' @export
+get_fnch_rider_gwp <- function(riderid) {
+  url <- glue::glue("https://info.fnch.ch/resultate/reiter/{riderid}.json?tab=gwp")
+  info <- jsonlite::fromJSON(url) %>%
+    dplyr::select(1:2)
+
+  return(info)
+}
+
 #' Get jumping results for riderid
 #'
 #' @param riderid A licence id for a rider

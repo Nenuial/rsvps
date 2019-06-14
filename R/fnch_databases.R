8bdbc167c913ff6be2232e209a266039415ce323
Nenuial <pascal.burkhard@gmail.com>
2019-06-14 14:07:04 +0200

    add database rider functions

--- R/fnch_databases.R
+++ R/fnch_databases.R
@@ -13,3 +13,45 @@ get_fnch_riders <- function() {
 get_fnch_horses <- function() {
   return(pkg.env$horses)
 }
+
+#' Get rider address
+#'
+#' @param license A rider license id
+#'
+#' @return A string with rider name and address
+#' @export
+get_fnch_rider_address <- function(license) {
+  rider <- get_fnch_riders() %>% dplyr::filter(Licence == license)
+
+  return_text <- glue::glue("{rider %>% dplyr::pull(Licence)} {rider %>% dplyr::pull(FirstName)} {rider %>% dplyr::pull(LastName)} = {rider %>% dplyr::pull(Address)}, {rider %>% dplyr::pull(ZIP)} {rider %>% dplyr::pull(Place)}")
+
+  return(return_text)
+}
+
+#' Get rider email
+#'
+#' @param license A rider license id
+#'
+#' @return A string with rider name and email
+#' @export
+get_fnch_rider_email <- function(license) {
+  rider <- get_fnch_riders() %>% dplyr::filter(Licence == license)
+
+  return_text <- glue::glue("{rider %>% dplyr::pull(FirstName)} {rider %>% dplyr::pull(LastName)} <{rider %>% dplyr::pull(Email)}>")
+
+  return(return_text)
+}
+
+#' Get rider phone
+#'
+#' @param license A rider license id
+#'
+#' @return A string with rider name and mobile phone
+#' @export
+get_fnch_rider_mobile <- function(license) {
+  rider <- get_fnch_riders() %>% dplyr::filter(Licence == license)
+
+  return_text <- glue::glue("{rider %>% dplyr::pull(FirstName)} {rider %>% dplyr::pull(LastName)} <{rider %>% dplyr::pull(PhoneM)}>")
+
+  return(return_text)
+}

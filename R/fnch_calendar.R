0b8aefd78de82ad65b9b125a126fad451c2a10a7
Pascal Burkhard <pascal.burkhard@gmail.com>
2020-10-20 14:03:29 +0200

    Remove newlines from descriptions

--- R/fnch_calendar.R
+++ R/fnch_calendar.R
@@ -55,8 +55,8 @@ get_fnch_ics_calendar <- function(start, end, federation, with_links = F) {
                   End = format(bis, format = "%Y/%m/%d"),
                   Summary = glue::glue("{typ_code} {ort} ({kanton})"),
                   Description = ifelse(disp_links,
-                                       glue::glue("{stringr::str_trim(vorgesehene_pruefungen)}<br/><a href='https://info.fnch.ch/#/veranstaltungskalender/ausschreibung/{id}' target='_blank'>Lien vers le portail FNCH</a>{website_text}"),
-                                       glue::glue("{stringr::str_trim(vorgesehene_pruefungen)}")),
+                                       glue::glue("{stringr::str_trim(stringr::str_replace_all(vorgesehene_pruefungen, '[\r\n]' , ' '))}<br/><a href='https://info.fnch.ch/#/veranstaltungskalender/ausschreibung/{id}' target='_blank'>Lien vers le portail FNCH</a>{website_text}"),
+                                       glue::glue("{stringr::str_trim(stringr::str_replace_all(vorgesehene_pruefungen, '[\r\n]' , ' '))}")),
                   URL = glue::glue("https://info.fnch.ch/#/veranstaltungskalender/ausschreibung/{id}")) %>%
     dplyr::select(Start, End, Summary, Description, URL) %>%
     purrr::pmap_dfr(create_ics_cal) %>%

fc8a262f36483601d9f218dcde7231344c049798
Pascal Burkhard <pascal.burkhard@gmail.com>
2025-09-21 10:01:31 +0200

    fix: whatever

--- R/aen_jumping.R
+++ R/aen_jumping.R
@@ -6,21 +6,33 @@
 #'
 #' @return The table with data
 #' @export
-aen_get_championship_data <- function(table, last_championship, engagement_deadline) {
+aen_get_championship_data <- function(
+  table,
+  last_championship,
+  engagement_deadline
+) {
   table |>
     dplyr::filter(!kein_start) |>
     dplyr::rowwise() |>
-    dplyr::mutate(data = list(aen_get_championship_infos(reiter_id, pferd_id,
-                                                         last_championship, engagement_deadline))) |>
+    dplyr::mutate(
+      data = list(aen_get_championship_infos(
+        reiter_id,
+        pferd_id,
+        last_championship,
+        engagement_deadline
+      ))
+    ) |>
     tidyr::unnest_wider(data) |>
     dplyr::select(
-      c(reiter_id,
+      c(
+        reiter_id,
         reiter_vorname,
         reiter_name,
         mobile,
         pferd_id,
         pferd_name,
         lic,
+        dplyr::starts_with("par"),
         dplyr::starts_with("res"),
         dplyr::starts_with("ex"),
         sop
@@ -37,12 +49,20 @@ aen_get_championship_data <- function(table, last_championship, engagement_deadl
 #'
 #' @return A list to be expanded in championship table
 #' @export
-aen_get_championship_infos <- function(rider_id, horse_id, last_championship, engagement_deadline) {
+aen_get_championship_infos <- function(
+  rider_id,
+  horse_id,
+  last_championship,
+  engagement_deadline
+) {
   rider_id |>
     rsvps::get_fnch_rider_infos() -> rider_infos
 
   rider_id |>
     rsvps::get_my_fnch_rider_details() -> rider_details
+
+  rider_id |>
+    rsvps::get_fnch_rider_jumping_results() -> rider_results
 
   horse_id |>
     rsvps::get_fnch_horse_jumping_results() -> horse_results
@@ -50,8 +70,19 @@ aen_get_championship_infos <- function(rider_id, horse_id, last_championship, en
   # Height for all results
   horse_results |>
     dplyr::filter(klassiert) |>
-    dplyr::mutate(height = readr::parse_number(kategorie_code, na = c("SP/CS"))) |>
+    dplyr::mutate(
+      height = readr::parse_number(kategorie_code, na = c("SP/CS"))
+    ) |>
     tidyr::drop_na(height) -> horse_results_clean
+
+  # Starts = RIDER starts
+  rider_results |>
+    dplyr::filter(datum > last_championship) |>
+    dplyr::mutate(
+      height = readr::parse_number(kategorie_code, na = c("SP/CS"))
+    ) |>
+    tidyr::drop_na(height) |>
+    dplyr::summarise(par90 = sum(height > 90)) -> starts
 
   # Qualifications = HORSE + RIDER results
   horse_results_clean |>
@@ -59,35 +90,53 @@ aen_get_championship_infos <- function(rider_id, horse_id, last_championship, en
       datum > last_championship,
       reiter_id == rider_id
     ) |>
-    dplyr::summarise(res90 = sum(height == 90 | height == 95),
-                     res100 = sum(height == 100 | height == 105),
-                     res110 = sum(height == 110 | height == 115),
-                     res120 = sum(height == 120 | height == 125),
-                     res130 = sum(height == 130 | height == 135),
-                     res140 = sum(height >= 140),
-                     res110p = sum(height >= 110),
-                     res120p = sum(height >= 120)) |>
-    dplyr::mutate(dplyr::across(dplyr::everything(), ~tidyr::replace_na(.x, 0))) -> qualifications
+    dplyr::summarise(
+      res90 = sum(height == 90 | height == 95),
+      res100 = sum(height == 100 | height == 105),
+      res110 = sum(height == 110 | height == 115),
+      res120 = sum(height == 120 | height == 125),
+      res130 = sum(height == 130 | height == 135),
+      res140 = sum(height >= 140),
+      res110p = sum(height >= 110),
+      res120p = sum(height >= 120)
+    ) |>
+    dplyr::mutate(dplyr::across(
+      dplyr::everything(),
+      ~ tidyr::replace_na(.x, 0)
+    )) -> qualifications
 
   horse_results_clean |>
     dplyr::filter(
       lubridate::year(datum) >= lubridate::year(engagement_deadline) - 1
     ) |>
-    dplyr::summarise(ex135 = sum(height >= 135),
-                     exN135 = sum(height >= 140 | kategorie_code == "N135"),
-                     ex140 = sum(height >= 140 )) |>
-    dplyr::mutate(dplyr::across(dplyr::everything(), ~tidyr::replace_na(.x, 0))) -> exclusions
+    dplyr::summarise(
+      ex135 = sum(height >= 135),
+      exN135 = sum(height >= 140 | kategorie_code == "N135"),
+      ex140 = sum(height >= 140)
+    ) |>
+    dplyr::mutate(dplyr::across(
+      dplyr::everything(),
+      ~ tidyr::replace_na(.x, 0)
+    )) -> exclusions
 
   horse_results_clean |>
     dplyr::filter(
       lubridate::year(datum) >= lubridate::year(engagement_deadline) - 1,
       datum <= engagement_deadline
     ) |>
-    dplyr::summarise(sop = sum(gwp)) |>
-    dplyr::mutate(dplyr::across(dplyr::everything(), ~tidyr::replace_na(.x, 0))) -> sop
+    dplyr::summarise(sop = sum(gwp, na.rm = TRUE)) |>
+    dplyr::mutate(dplyr::across(
+      dplyr::everything(),
+      ~ tidyr::replace_na(.x, 0)
+    )) -> sop
 
-
-  return(c(list(mobile = rider_details$mobile, lic = rider_infos$lizenzen), qualifications |> as.list(), exclusions |> as.list(), sop |> as.list()))
+  return(c(
+    list(mobile = rider_details$mobile, lic = rider_infos$lizenzen),
+    starts |> as.list(),
+    qualifications |> as.list(),
+    exclusions |> as.list(),
+    sop |> as.list()
+  ))
 }
 
 #' Check qualifications for B/R Championship
@@ -110,7 +159,7 @@ aen_check_championship_br <- function(table) {
   table |>
     dplyr::mutate(
       checkout = dplyr::if_else(
-        (res90 + res100) >= 2,
+        par90 >= 2,
         TRUE,
         FALSE
       )
@@ -139,9 +188,6 @@ aen_check_championship_br <- function(table) {
 #' @export
 aen_check_championship_r <- function(table) {
   # Championnat R
-  # Critères qualification PAIRE (depuis dernier championnat) :
-  # * classements 100/105  : 5
-  # * classements 110/plus : 2
   #
   # Critères d'exclusion PAIRE (depuis dernier championnat) :
   # * plus de 3 classements (>= 4) 120+
@@ -150,13 +196,14 @@ aen_check_championship_r <- function(table) {
   # * classement 140+
 
   table |>
-    dplyr::mutate(
-      checkout = dplyr::if_else(
-        res100 >= 5 | res110p >= 2 | (res100 + res110p * 2) >= 5,
-        TRUE,
-        FALSE
-      )
-    ) |>
+    # dplyr::mutate(
+    #   checkout = dplyr::if_else(
+    #     res100 >= 5 | res110p >= 2 | (res100 + res110p * 2) >= 5,
+    #     TRUE,
+    #     FALSE
+    #   )
+    # ) |>
+    dplyr::mutate(checkout = TRUE) |>
     dplyr::mutate(
       checkout = dplyr::if_else(
         res120p > 3,
@@ -182,23 +229,13 @@ aen_check_championship_r <- function(table) {
 #' @export
 aen_check_championship_rn <- function(table) {
   # Championnat R/N
-  # Critères qualification PAIRE (depuis dernier championnat) :
-  # * classements 110/115  : 5
-  # * classements 120/125  : 2
-  # * classements 130/plus : 1
   #
   # Critères de catégorie :
   # * licence N et plus de 1500 SoP => 135
   # * cheval classé N 135 dans l'année en cours et l'année précédente => 135
 
   table |>
-    dplyr::mutate(
-      checkout = dplyr::if_else(
-        res110 >= 5 | res120 >= 2 | res130 >= 1 | res140 >= 1 | (res110 + res120 * 2) >= 5,
-        TRUE,
-        FALSE
-      )
-    )  |>
+    dplyr::mutate(checkout = TRUE) |>
     dplyr::mutate(
       category = dplyr::if_else(
         (stringr::str_detect(lic, "SN") & sop > 1500) | exN135 > 0,
@@ -207,6 +244,3 @@ aen_check_championship_rn <- function(table) {
       )
     )
 }
-
-
-
